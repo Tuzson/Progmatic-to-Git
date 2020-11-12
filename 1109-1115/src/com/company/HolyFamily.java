f9ca10ab8b9849384adace29331e717f580ac7be
Tuzson <kecseyt@gmail.com>
2020-11-12 19:38:46 +0100

    HolyFamily gyakorlás Öröklődés

---
+++ 1109-1115/src/com/company/HolyFamily.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class HolyFamily {
+    public static void main(String[] args) {
+        Father father = new Son();
+    }
+
+}
+class Logos {
+
+    public Logos() {
+        System.out.println("I am the Logos");
+    }
+}
+
+class Father extends Logos {
+    public Father() {
+        System.out.println("I am the Father");
+    }
+}
+
+class Son extends Father{
+    public Son() {
+        System.out.println("I am the son");
+    }
+}
+

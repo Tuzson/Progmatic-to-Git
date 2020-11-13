c5063399fe2a4dcb790c7ce3c5d706248887128a
Tuzson <kecseyt@gmail.com>
2020-11-13 11:13:39 +0100

    Öröklődés gyakolás, City class létrehozva, kibővítve, ElectricBike osztály létrehozva

---
+++ 1109-1115/src/com/company/CityBike.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class CityBike extends BikeÖröklődés {
+    boolean light;
+    String horn;
+
+    public CityBike(double wheelDiameter, int frontCogWheelCogNr, int[] rearCogWheelCogNrs) {
+        super(wheelDiameter, frontCogWheelCogNr, rearCogWheelCogNrs);
+    }
+
+    public void switchLight (){
+        light= !light;
+    }
+    public void isLighting (){
+        System.out.println(light);
+    }
+    public void horning(){
+        horn = "Csing csing";
+        System.out.println(horn);
+    }
+
+
+
+}
--- 1109-1115/src/com/company/Main.java
+++ 1109-1115/src/com/company/Main.java
@@ -3,5 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        CityBike cityBike = new CityBike(28,30,new int[]{12,14,16,18,20,22});
+        cityBike.isLighting();
+        cityBike.switchLight();
+        cityBike.isLighting();
+        cityBike.horning();
+
     }
 }

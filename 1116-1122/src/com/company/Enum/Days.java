4cf0a65cf0f383ce4d77e29b20cb5bcf0b1b49fc
Tuzson <kecseyt@gmail.com>
2020-11-19 09:31:01 +0100

    Enum practice

---
+++ 1116-1122/src/com/company/Enum/Days.java
@@ -0,0 +1,11 @@
+package com.company.Enum;
+
+public enum Days {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ 1116-1122/src/com/company/Enum/Main.java
@@ -0,0 +1,9 @@
+package com.company.Enum;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Days days = Days.SATURDAY;
+    }
+}

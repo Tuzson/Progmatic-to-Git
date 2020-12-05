fe9d5cce8ec0a59da56e41dc7523450da039e94b
Tuzson <kecseyt@gmail.com>
2020-12-05 20:21:29 +0100

    String Practice

---
+++ 1116-1122/src/com/company/StringPractice.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class StringPractice implements CharSequence {
+
+    public StringPractice(String s) {
+
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return null;
+    }
+}
